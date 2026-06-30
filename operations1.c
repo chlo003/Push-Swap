4fd0f56bca7805ee47f4cb3a2b9353b6cfdf473d
chlminga <chlminga@f6r8s8.paris.42.school>
2026-06-30 19:02:38 +0200

    modif 30/06

--- operations1.c
+++ operations1.c
@@ -6,50 +6,123 @@
 /*   By: chlminga <chlminga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/29 15:41:16 by chlminga          #+#    #+#             */
-/*   Updated: 2026/06/29 18:57:39 by chlminga         ###   ########.fr       */
+/*   Updated: 2026/06/30 18:07:49 by chlminga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 
-t_node	create_node(int data)
+t_node	*create_node(int data)
 {
 	t_node	*new;
 
 	new = malloc(sizeof(t_node));
-	if (new == NULL)
+	if (!new)
 		return (NULL);
 	new->data = data;
 	new->next = NULL;
-	new->prev = NULL;
 	return (new);
 }
 
-t_node	swap_last(t_node **tail)
+t_node	*ft_lstlast(t_node *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
+
+void	ft_lstadd_back(t_node **lst, t_node *new)
+{
+	if (!lst || !new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	ft_lstlast(*lst)->next = new;
+}
+
+
+
+t_node	swap_a(t_node **head)
 {
 	t_node	*temp;
 	t_node	*current;
 
-	current = *tail;
-	temp = NULL;
-	while (current != NULL)
-	{
-		temp = current;
-		current = current->prev;
-		current->prev = temp;
-	}
-	if (t_node)
+	current = *head;
+	temp = current;
+	current->data = current->next->data;
+	printf("%d\n", temp->data);
+	current->next->data = temp->data;
+	// printf("%d", current->next->data);
+	printf("sa\n");
 }
 
-int main(int ac, char **av)
+t_node	swap_b(t_node **head)
 {
-	t_node	stack_a;
-	t_node	stack_b;
+	t_node	*temp;
+	t_node	*current;
 
-	stack_a = NULL;
-	stack_b = NULL;
-	if (ac < 2)
-		return (0);
+	current = *head;
+	temp = NULL;
+	temp = current;
+	current = current->next;
+	current->next = temp;
+	printf("sb\n");
+}
+
+// t_node	push(t_node **, t_node **)
+// {
+
+// }
+
+int main(void)
+{
+    t_node *head = NULL;
+    t_node *second = NULL;
+    t_node *third = NULL;
+	printf("1");
+	head = create_node(10);
+	ft_lstadd_back(&head, create_node(20));
+	ft_lstadd_back(&head, create_node(30));
+	printf("%d \n", head->next->data);
+    // head->next = second;
+	// printf("2");
+    // second = create_node(20);
+    // second->next = third;
+	// printf("3");
+    // third = create_node(30);
+    // third->next = NULL;
+
+    // // Print linked list
+    t_node *temp = head;
+	// printf("First");
+	printf("%d -> %d -> %d", head->data, head->next->data, head->next->next->data);
+	swap_a(&head);
+	printf("%d -> %d -> %d", head->data, head->next->data, head->next->next->data);
+	// printf("Second");
+    // while (temp != NULL)
+    // {
+    //     printf("%d -> ", temp->data);
+    //     temp = temp->next;
+    // }
+	// printf("Third");
+    // printf("NULL\n");
+
+    // // Free allocated memory
+    // temp = head;
+    // while (temp != NULL)
+    // {
+    //     t_node *next = temp->next;
+    //     free(temp);
+    //     temp = next;
+    // }
+
+    return 0;
 }
--- push_swap.h
+++ push_swap.h
@@ -1,13 +1,14 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-t_node	create_node(int data);
+
 
 typedef struct s_node
 {
 	int				data;
 	struct s_node	*next;
-	struct s_node	*prev;
-}	t_node;
+}					t_node;
+
+t_node	*create_node(int data);
 
 #endif
